427d921074f4423934e336813e4b0431bb5adc32
mustafaberk-ay <mustafaberkay5234@gmail.com>
2024-01-25 18:53:26 +0300

    add getHighestThreadIdService in messageService.ts

--- api/src/services/messageService.ts
+++ api/src/services/messageService.ts
@@ -1,14 +1,28 @@
-import Message, { MessageDocument } from "../models/Message";
+import Message, { MessageDocument } from '../models/Message';
 
-export const getMessagesService = async (): Promise<MessageDocument[] | undefined> => {
-	return Message.find()
-		.then((data: MessageDocument[]) => data)
-}
+export const getMessagesService = async (): Promise<
+	MessageDocument[] | undefined
+> => {
+	return Message.find().then((data: MessageDocument[]) => data);
+};
 
-export const createMessageService = async (message : MessageDocument): Promise<MessageDocument | undefined> => {
-	return message.save()
-}
+export const createMessageService = async (
+	message: MessageDocument
+): Promise<MessageDocument | undefined> => {
+	return message.save();
+};
 
-export const getMessageByIdService = async (id: string): Promise<MessageDocument | null> => {
-	return Message.findById(id)
-}
+export const getMessageByIdService = async (
+	id: string
+): Promise<MessageDocument | null> => {
+	return Message.findById(id);
+};
+
+export const getHighestThreadIdService = async (): Promise<number | null> => {
+	const result = await Message.findOne({}, {}, { sort: { thread_id: -1 } });
+	if (result) {
+		return result.thread_id;
+	} else {
+		return null; // No documents found
+	}
+};
