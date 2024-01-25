45479fc97516eb1ea7c51f2b0f1e9d7458c46bd1
mustafaberk-ay <mustafaberkay5234@gmail.com>
2024-01-25 18:31:30 +0300

    connect to mongodb in server.ts

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,8 +1,20 @@
 import express from 'express';
 import app from './app';
+import mongoose from 'mongoose';
 
 const port = 3000;
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+
+
+mongoose.connect(process.env.MONGODB_URL as string)
+    .then(() => {
+        app.listen(port, () => {
+            console.log(`server is running on port ${port}`)
+        })
+    })
+    .catch((err: Error) => {
+        console.log(
+            'MongoDB connection error. Please make sure the database is running!'
+        )
+        process.exit(1)
+    })
