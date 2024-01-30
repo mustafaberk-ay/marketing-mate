7bd46949ed123d5273243aba9b56542972de3899
mustafaberk-ay <mustafaberkay5234@gmail.com>
2024-01-30 21:11:37 +0300

    delete chatCompletion.ts file

--- api/src/routes/chatCompletion.ts
+++
@@ -1,6 +0,0 @@
-import { Router } from "express";
-import { getMessagesFromThread } from "../controllers/chatCompletion";
-
-export const chatCompletionRouter = Router();
-
-chatCompletionRouter.post('/getMessagesFromThread', getMessagesFromThread);
