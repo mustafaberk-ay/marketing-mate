3046b4786e06896ea83596f569f9117c25195f86
mustafaberk-ay <mustafaberkay5234@gmail.com>
2024-02-21 19:39:41 +0300

    add user_id in UserInfo model

--- api/src/models/UserInfo.ts
+++ api/src/models/UserInfo.ts
@@ -5,13 +5,15 @@ export type UserInfoDocument = Document & {
     gmail_address: string;
     gmail_app_password: string;
     assistant_phone_number: string;
+    user_id: string;
 }
 
 export const UserInfoSchema = new mongoose.Schema({
     facebook_user_access_token: {type: String},
     gmail_address: {type: String},
     gmail_app_password: {type: String},
-    assistant_phone_number: {type: String}
+    assistant_phone_number: {type: String},
+    user_id: {type: String, required: true}
 })
 
 export default mongoose.model<UserInfoDocument>('userInfo', UserInfoSchema)
