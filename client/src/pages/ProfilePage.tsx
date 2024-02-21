2f419e6759f620e6a644d91d20c2ca276c56e344
mustafaberk-ay <mustafaberkay5234@gmail.com>
2024-02-21 23:29:52 +0300

    fetch user info from db in profile page

--- client/src/pages/ProfilePage.tsx
+++ client/src/pages/ProfilePage.tsx
@@ -1,12 +1,45 @@
 import { useAuth0 } from '@auth0/auth0-react';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { UserInfo } from '../type';
+import { decryptData } from '../utils/encryptionUtils';
 
 interface ProfilePageProps {
 	token: string;
+	userId: string;
 }
 
-const ProfilePage: React.FC<ProfilePageProps> = ({ token }) => {
+const ProfilePage: React.FC<ProfilePageProps> = ({ token, userId }) => {
+	const [userInfo, setUserInfo] = useState<UserInfo | null>(null);
 	const { user, isAuthenticated, isLoading } = useAuth0();
+
+	useEffect(() => {
+		async function getUserInfo() {
+			await fetch('http://localhost:3000/user-info/getUserInfoById', {
+				method: 'POST',
+				headers: { 'Content-Type': 'application/json' },
+				body: JSON.stringify({ id: userId }),
+			})
+				.then((response) => {
+					if (!response.ok) {
+						throw new Error('Network response was not ok');
+					}
+					return response.json();
+				})
+				.then((data) => {
+					setUserInfo(data)
+					console.log(data, 'data');
+				})
+				.catch((error) => {
+					console.error(
+						'There was a problem with your fetch operation:',
+						error
+					);
+				});
+		}
+
+		getUserInfo();
+	});
 
 	if (isLoading) {
 		return <div>Loading ...</div>;
@@ -17,16 +50,13 @@ const ProfilePage: React.FC<ProfilePageProps> = ({ token }) => {
 	// const uid = info?.at(1);
 
 	return (
-		isAuthenticated && user && (
+		userInfo && (
 			<div>
-				<img
-					src={user.picture}
-					alt={user.name}
-				/>
-				<h2>{user.name}</h2>
-				<p>{user.email}</p>
-				<p>{user.sub}</p>
-				<p>{token}</p>
+				<p>ID: {userInfo._id}</p>
+				<p>Facebook Token: {userInfo.facebook_user_access_token}</p>
+				<p>Gmail Address: {userInfo.gmail_address}</p>
+				<p>Gmail App Password: {userInfo.gmail_app_password}</p>
+				<p>Assistant Phone Number: {decryptData(userInfo.assistant_phone_number) }</p>
 				<Link to='/'>Go Back</Link>
 			</div>
 		)
