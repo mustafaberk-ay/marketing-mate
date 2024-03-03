c745a7c4a7b39468285fa0bed40e9fc52e160d28
mustafaberk-ay <mustafaberkay5234@gmail.com>
2024-03-03 17:20:00 +0100

    create pages and UI for Message Marketing pages

---
+++ client/src/pages/messageMarketing/MessageMarketingPage3.tsx
@@ -0,0 +1,86 @@
+import { useAuth0 } from '@auth0/auth0-react';
+import Navbar from '../../components/Navbar';
+import NotLoggedIn from '../../components/NotLoggedIn';
+import loginImage from '/login.png';
+import shareImage from '/share.png';
+import PrevStepButton from '../../components/PrevStepButton';
+import CompleteButton from '../../components/CompleteButton';
+
+function MessageMarketingPage3() {
+	const { isAuthenticated } = useAuth0();
+
+	function sendMessageButtonOnClick() {
+		console.log('sendMessageButtonOnClick');
+	}
+
+	function setupWhatsappButtonOnClick() {
+		console.log('setupWhatsappButtonOnClick');
+	}
+
+	return (
+		<div>
+			{isAuthenticated ? (
+				<div>
+					<Navbar />
+					<div className='flex flex-col space-y-4 pl-10 pr-10 pt-2'>
+						<div className='font-semibold text-white text-center text-5xl'>
+							Message Marketing
+						</div>
+
+						<div className='text-white text-3xl '>Step 3: Send Message</div>
+
+						<div className='text-lightBrown text-center text-2xl pt-10'>
+							Enter the name of the contact, you would like to send your
+							marketing message.
+						</div>
+
+						<div className='flex space-x-8 justify-center'>
+							<label className='text-white text-5xl mr-16 font-extrabold'>
+								Contact Name:
+							</label>
+							<textarea
+								className='border-4 bg-darkBlue text-white border-darkBrown rounded-md focus:outline-none focus:border-lightBrown w-1/5 text-2xl'
+								wrap='soft'
+							></textarea>
+						</div>
+
+						<div className='text-lightBrown text-center text-2xl pt-10'>
+							You need to setup WhatsApp before sending a message
+						</div>
+
+						<div className='flex justify-center space-x-28'>
+							<button
+								className='flex justify-around items-center bg-darkBrown text-white py-5 rounded-50 w-96 h-20 text-3xl transition-transform hover:scale-110'
+								onClick={setupWhatsappButtonOnClick}
+							>
+								<img
+									className='h-8'
+									src={loginImage}
+								/>
+								Setup WhatsApp
+							</button>
+							<button
+								className='flex justify-around items-center bg-darkBrown text-white py-5 rounded-50 w-96 h-20 text-3xl transition-transform hover:scale-110'
+								onClick={sendMessageButtonOnClick}
+							>
+								<img
+									className='h-8'
+									src={shareImage}
+								/>
+								Send Message
+							</button>
+						</div>
+						<div className='flex justify-between pt-9'>
+							<PrevStepButton prevStepPath='/message-marketing-2' />
+							<CompleteButton />
+						</div>
+					</div>
+				</div>
+			) : (
+				<NotLoggedIn />
+			)}
+		</div>
+	);
+}
+
+export default MessageMarketingPage3;
