34f4900d314abc5c9d627b088ba5e122927c8129
mustafaberk-ay <mustafaberkay5234@gmail.com>
2024-03-07 17:17:24 +0100

    remove mongodb from server

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,18 +1,7 @@
-import express from 'express';
 import app from './app';
-import mongoose from 'mongoose';
 
 const port = 3000;
 
-mongoose.connect(process.env.MONGODB_URL as string)
-    .then(() => {
-        app.listen(port, () => {
-            console.log(`server is running on port ${port}`)
-        })
-    })
-    .catch((err: Error) => {
-        console.log(
-            'MongoDB connection error. Please make sure the database is running!'
-        )
-        process.exit(1)
-    })
+app.listen(port, () => {
+	console.log(`server is running on port ${port}`);
+});
