5cdd9a7e761107fbe43d9d564899ad95603a5bfb
mustafaberk-ay <mustafaberkay5234@gmail.com>
2024-03-09 04:29:29 +0100

    configure store for productDetails

--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './slices/userInfoSlice';
+import productDetailsReducer from './slices/productDetailsSlice'
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
+    productDetails: productDetailsReducer
   },
 });
 
