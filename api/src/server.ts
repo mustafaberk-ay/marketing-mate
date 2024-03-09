0fe84621bb38a5aed7ceb55990053b0f345d5990
mustafaberk-ay <mustafaberkay5234@gmail.com>
2024-03-09 04:27:09 +0100

    retrieve marketing mate assistant once the backend starts running

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,7 +1,9 @@
 import app from './app';
+import { retrieveMarketingMateAssistantService } from './services/chatThread';
 
 const port = 3000;
 
 app.listen(port, () => {
 	console.log(`server is running on port ${port}`);
+	retrieveMarketingMateAssistantService()
 });
