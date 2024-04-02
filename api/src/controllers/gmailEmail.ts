d0e462061e8c256f3e4e5323b71132f4a7df034d
mustafaberk-ay <mustafaberkay5234@gmail.com>
2024-04-02 17:03:20 +0200

    decrypt app password once the sendGmailEmail endpoint is called

--- api/src/controllers/gmailEmail.ts
+++ api/src/controllers/gmailEmail.ts
@@ -1,10 +1,12 @@
 import { Request, Response } from 'express';
 import { sendGmailEmailService } from '../services/gmailEmail';
+import { decryptData } from '../utils/encryptionUtils';
 
 export const sendGmailEmail = async (req: Request, res: Response) => {
 	try {
 		const { sender, to, message, subject, appPassword } = req.body;
-		const response = await sendGmailEmailService(sender, to, message, subject, appPassword);
+		const decryptedAppPassword = decryptData(appPassword)
+		const response = await sendGmailEmailService(sender, to, message, subject, decryptedAppPassword);
 		res.status(200).json(response);
 	} catch (error) {
 		res.status(500).json(error);
